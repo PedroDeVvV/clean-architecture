9cdc8c95eadc69d8a12585dbd80e2f78909726c8
PedroDeVvV <pedrohgs2004@gmail.com>
2025-04-22 10:15:43 -0300

    FEAT: add listing of users

--- src/main/java/br/com/alura/codechella/config/UsuarioConfig.java
+++ src/main/java/br/com/alura/codechella/config/UsuarioConfig.java
@@ -3,6 +3,7 @@ package br.com.alura.codechella.config;
 
 import br.com.alura.codechella.application.gateways.RepositorioDeUsuario;
 import br.com.alura.codechella.application.usecases.CriarUsuario;
+import br.com.alura.codechella.application.usecases.ListarUsuarios;
 import br.com.alura.codechella.infra.gateways.RepositorioDeUsuarioJPA;
 import br.com.alura.codechella.infra.gateways.UsuarioEntityMapper;
 import br.com.alura.codechella.infra.persistence.UsuarioRepository;
@@ -26,5 +27,10 @@ public class UsuarioConfig {
     @Bean
     UsuarioEntityMapper retornaMapper() {
         return new UsuarioEntityMapper();
+    }
+
+    @Bean
+    ListarUsuarios listarUsuarios(RepositorioDeUsuario repositorio) {
+        return new ListarUsuarios(repositorio);
     }
 }
--- src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJPA.java
+++ src/main/java/br/com/alura/codechella/infra/gateways/RepositorioDeUsuarioJPA.java
@@ -6,6 +6,7 @@ import br.com.alura.codechella.infra.persistence.UsuarioEntity;
 import br.com.alura.codechella.infra.persistence.UsuarioRepository;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RepositorioDeUsuarioJPA implements RepositorioDeUsuario {
 
@@ -26,7 +27,8 @@ public class RepositorioDeUsuarioJPA implements RepositorioDeUsuario {
 
     @Override
     public List<Usuario> listarTodos() {
-        //return repositorio.findAll();
-        return null;
+        return repositorio.findAll().stream()
+                .map(mapper::toDomain)
+                .collect(Collectors.toList());
     }
 }
